37da425b6b1db4673dabcfa7120bc2b4c0b1c865
Gbolu Adeyemi <gboluwagaadeyemi@gmail.com>
2023-09-06 16:39:25 +0100

    chore: add rand with string parsing

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,11 +1,25 @@
+use rand::{self, Rng};
 use std::io;
 
 fn main() {
     println!("Guess the number!");
     println!("Please input your guess.");
     let mut guess = String::new();
+
+    let secret_number = rand::thread_rng().gen_range(1..10); // Generate a random number
+
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
-    println!("You guessed: {}", guess);
+
+    let mut guess_message: &str = "incorrect";
+
+    if secret_number == guess.trim().parse::<u32>().unwrap() {
+        guess_message = "correct"
+    }
+
+    println!(
+        "You guess: {} was {}. Right value was {}",
+        guess, guess_message, secret_number
+    );
 }
